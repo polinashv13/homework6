7996545fbce2d4715a99b52feced277f74faed42
polinashv13 <114211562+polinashv13@users.noreply.github.com>
2022-10-21 07:50:38 +0300

    Add files via upload

--- cp6.py
+++ cp6.py
@@ -2,8 +2,12 @@ import random
 r = int(input("Ведите размерность массива: "))
 masr = []
 print("Введите элементы массива")
-for i in range(r):
-    masr.append(round(random.uniform(0,9),2))
+def massive(t):
+    masr = []
+    for i in range(t):
+        masr.append(round(random.uniform(0,9),2))
+    return masr
+masr = massive(r)
 m = max(masr)
 ind = masr.index(m)
 for i in range(ind+1,r):
